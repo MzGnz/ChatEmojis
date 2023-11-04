5bc71d7d15fd4414b32aa127aa74d41645cc313a
Javier Calixto <Javier.Alejandro.Calixto.Cano1@ibm.com>
2023-11-03 18:06:08 -0600

    Adicion de prueba de emojis

---
+++ src/stomp/stomp.js
@@ -0,0 +1,9 @@
+import * as Stomp from 'stompjs'
+
+//const ws = new WebSocket("ws://localhost:15674/ws")
+const ws = new WebSocket("ws://rabbitmq-stomp.fly.dev:15674/ws")
+const stomp = Stomp.over(ws)
+
+export const connectStomp = () => new Promise((resolve, reject) => {
+    stomp.connect('guest', 'guest', () => resolve(stomp), () => reject(), '/')
+})
